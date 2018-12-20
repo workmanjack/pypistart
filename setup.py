d0ecfaf9facf5e220ecf2d65690a6ae87888a6ab
workmanjack <jworkman16@gmail.com>
2018-12-20 14:23:36 -0800

    updated description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pypistart',
     version='0.1.0',
-    description='Starter kit for initializing a python package skeleton',
+    description='Starter kit for initializing a python pypi package',
     long_description=open('README.md').read(),
     author='Jack Workman',
     author_email='jworkman16@gmail.com',
